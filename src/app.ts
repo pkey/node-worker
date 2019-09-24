2cd92be784db389c57a13dc73be3eab9776ac887
pkey <kutka.paulius@gmail.com>
2019-09-24 18:58:19 +0300

    Ready for development

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-require("dotenv").config();
+import * as dotenv from "dotenv";
+import Koa from "koa";
+import Router from "koa-router";
+
+dotenv.config();
+
+const app = new Koa();
+const router = new Router();
+
+const port = process.env.PORT || 3000;
+
+router.get("/*", async ctx => {
+  ctx.body = "Hello World!";
+});
+
+app.use(router.routes());
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));

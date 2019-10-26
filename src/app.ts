0dd794da886dd1de6bd517176900a218fc01f668
pkey <kutka.paulius@gmail.com>
2019-10-26 12:12:44 +0300

    Curling nodes based on list

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import Koa from "koa";
 import koaBody from "koa-body";
 import Router from "koa-router";
+var request = require("request");
 
 var os = require("os");
 
@@ -12,8 +13,26 @@ const router = new Router();
 
 const port = process.env.PORT || 3000;
 
-router.get("/*", async ctx => {
+const nodes = [];
+
+//Perform an action
+router.get("/", async ctx => {
+  nodes.forEach(node => {
+    request(`http://${node}/host`, function(error, response, body) {
+      console.log("error:", error); // Print the error if one occurred
+      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+      console.log("body:", body); // Print the HTML for the Google homepage.
+    });
+  });
+});
+
+router.get("/host", async ctx => {
   ctx.body = os.hostname();
+});
+
+router.post("/*", async ctx => {
+  console.log(ctx.request.body);
+  ctx.body = ctx.request.body;
 });
 
 app.use(koaBody());

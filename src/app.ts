f8e5d2e33d8180751264784de99612750edbd603
Tomas Eglinskas <tomase@wix.com>
2019-10-27 02:13:01 +0300

    With VM Set

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,55 @@
 import * as dotenv from "dotenv";
+dotenv.config()
+
 import Koa from "koa";
 import koaBody from "koa-body";
-import Router from "koa-router";
-import request from "request";
+import Router from "koa-router"; 
 import axios from "axios";
 import logger from "./client/logger";
-let os = require("os");
+import azure from "./client/azure";
 
-dotenv.config();
+const os = require("os");
 
 const app = new Koa();
 const router = new Router();
 
 const port = process.env.PORT || 3000;
 
-//User initialises a payload
+const RESOURCE_NAME = "GenXChallenge";
+const VM_SET_NAME = "Computation";
+
 router.get("/", async ctx => {
   logger.info("Request received! Starting calculations..");
 
-  // TODO: change to virtual VM sets
-  await new Promise(done => setTimeout(done, 1000));
+  const processing = async () => {
+    try {
+      const azureClient = await azure.azureClient();
+
+      logger.info("Creating VM Set..");
+      await azureClient.computeClient.virtualMachineScaleSets.beginCreateOrUpdate(
+        RESOURCE_NAME,
+        VM_SET_NAME,
+        {
+          location: "northeurope"
+        }
+      );
+      logger.info("Spinning up new VM Machines..");
+      await azureClient.computeClient.virtualMachineScaleSets.beginStart(
+        RESOURCE_NAME,
+        VM_SET_NAME
+      );
+
+      logger.info("Calculations completed, killing..");
+      await azureClient.computeClient.virtualMachineScaleSets.deleteMethod(
+        RESOURCE_NAME,
+        VM_SET_NAME
+      );
+    } catch (err) {
+      logger.error(err);
+    }
+  };
+
+  await processing()
 
   logger.info("Calculations finished..");
   logger.info("Contacting main node-controller..");
